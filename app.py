96543cb865203b562500f2fcd3c4c9680afab1ea
Nour Ayyoub <nour.ayyoub360@gmail.com>
2023-10-17 16:53:06 +0300

    add  validators  & changes on styles

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 
 Users = [
-    {"Name": "Nour", "Registration_number": "12027771", "College": "IT", "Department": "CS", "Gender": "Male", "Email": "nour.ayyob@gmail.com", "Phone": "0595192015", "Vehicle_Plate": "1234567"},
+    {"Name": "Nour Ayyoub", "Registration_number": "12027771", "College": "IT", "Department": "CS", "Gender": "Male", "Email": "nour.ayyob@gmail.com", "Phone": "0595192015", "Vehicle_Plate": "1234567"},
     {"Name": "Alice", "Registration_number": "22334455", "College": "Engineering", "Department": "Mechanical", "Gender": "Female", "Email": "alice@example.com", "Phone": "1234567890", "Vehicle_Plate": "9876543"},
     {"Name": "Bob", "Registration_number": "33445566", "College": "Business", "Department": "Finance", "Gender": "Male", "Email": "bob@example.com", "Phone": "9876543210", "Vehicle_Plate": "5432167"},
     {"Name": "Eva", "Registration_number": "44556677", "College": "Arts", "Department": "History", "Gender": "Female", "Email": "eva@example.com", "Phone": "5556667777", "Vehicle_Plate": "7654321"}
@@ -13,6 +13,7 @@ Users = [
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    message = None
     form = VehiclePlateForm()
     if form.validate_on_submit():
         user = next((user for user in Users if user["Vehicle_Plate"] == form.VehiclePlate.data), None)
@@ -20,14 +21,12 @@ def home():
             return redirect(url_for('profile', username=user["Name"]))
         else:
             message = "User not found!"
-    else:
-        message = None
     return render_template("home.htm", title_of_page="Home", css_file="home", form=form, message=message)
 
 @app.route("/profile/<username>")
 def profile(username):
     user = next((user for user in Users if user["Name"] == username), None)
-    return render_template("profile.htm", title_of_page="Profile", css_file="profile", User=user)
+    return render_template("profile.htm", title_of_page=user["Name"], css_file="profile", User=user)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
--- form.py
+++ form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired,Length
 
 class VehiclePlateForm(FlaskForm):
-    VehiclePlate = StringField('Vehicle Plate', validators=[InputRequired()])
+    VehiclePlate = StringField('Vehicle Plate', validators=[InputRequired(),Length(min=7, max=7)])
     submit = SubmitField('Search')
